1850158d20db1546e4658005f79f4b968e53e710
Saksham Agarwal <saksham.agarwal@bennett.edu.in>
2023-01-31 05:19:49 +0530

    added translation

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input')
+var outputDiv = document.querySelector("#output")
 
-console.log(txtInput);
+var serverURL = "	https://api.funtranslations.com/translate/minion.json";
 
-function clickEventHandler(){
-    console.log("Clicked");
-    console.log("input",txtInput.value)
+function getTranslationURL(input) {
+
+    return serverURL + "?" + "text=" + input;
 }
 
-btnTranslate.addEventListener("click", clickEventHandler)
+//catch error
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("Something wrong with the surver! Try again after some time")
+}
+
+function clickHandler() {
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
